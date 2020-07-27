4e7b60aa4a03e3b12f1e95ce4e998da0f143c3a0
Matt-Moo16 <mattrmoore2016@gmail.com>
2020-07-27 15:55:01 -0400

    update shopping list, added JS file

---
+++ index.js
@@ -0,0 +1,45 @@
+function addAnItem() {
+    $('#js-shopping-list-form').submit(event => {
+      event.preventDefault();
+      const itemInput = $('#shopping-list-entry');
+      const itemName = itemInput.val();
+      itemInput.val('');
+      const newListItem = `
+          <li>
+          <span class="shopping-item">${itemName}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>
+      `
+      $('ul').append(newListItem);
+    });
+  }
+  
+  function deleteButton() {
+      $('ul').on('click', '.shopping-item-delete', event => { 
+          $(event.currentTarget).closest('li').remove()
+      });
+  } 
+  
+  
+  function checkButton() {
+      $('ul').on('click', '.shopping-item-toggle', event => {
+         $(event.currentTarget).closest('li').find('.shopping-item').toggleClass('shopping-item__checked'); 
+  
+      });
+  
+  }
+  function setUpEventHandlers() {
+      checkButton()
+      deleteButton()
+      addAnItem()
+  }
+  $(setUpEventHandlers);
+  
+  
